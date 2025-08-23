3b893b971ded00ee5419f2473895c38057a93cdd
megshenoy6 <157152171+megshenoy6@users.noreply.github.com>
2025-08-22 21:09:32 -0700

    Update page.js

--- src/app/officers/page.js
+++ src/app/officers/page.js
@@ -35,7 +35,7 @@ export default function Officers() {
             
             <div className="my-10 bg-blue-600 bg-opacity-20 border-2 rounded-lg  border-blue-600 px-8 py-6">  
                 <h2 className="text-blue-600 text-2xl font-bold mb-6">Executive Team</h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
                     {officers.exec.map((officer) => (
                     <OfficerCard key={officer.name} {...officer} />
                     ))}
