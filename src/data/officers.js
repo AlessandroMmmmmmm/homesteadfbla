d7ab02d0c329b465a91591905bc5f49a51e51eec
megshenoy6 <157152171+megshenoy6@users.noreply.github.com>
2025-08-24 17:03:05 -0700

    quotes

--- src/data/officers.js
+++ src/data/officers.js
@@ -5,24 +5,28 @@ const officers = {
             "position": "President",
             "bio": "Hey guys! I'm Dorie, and I'm so excited to serve as your President this year! I love designing, listening to music, watching cdramas, and getting boba. If you ever need anything, please feel free to reach out! Can't wait to meet you all :)",
             "image": "Dorie",
+            "quote": "less is more",
         },
         {
             "name": "Jane Ser",
             "position": "Executive VP - Development",
             "bio": "Hi guys, I'm Jane and I will be serving as the EVP of Development for the upcoming school year! I like watching people perform music and sipping boba tea a little too often to be healthy. Hope to get to meet some of you soon!",
             "image": "Jane",
+            "quote": "The purpose of life is a life of purpose",
         },
         {
             "name": "Smyan Shanbhag",
             "position": "Executive VP - Competitions",
             "bio": "Hello yall, my name is Smyan Shanbhag and I'm your EVP of Competitions which means I oversee everything that has to do with competitions. I love sports, guitar, music, and the outdoors. Dont hesitate to hit me up if you have any questions relating to competitions whatsoever.",
             "image": "Smyan",
+            "quote": "I’m not so good with the advice. Can I interest you in a sarcastic comment?",
         },
         {
             "name": "Henry Hu",
             "position": "Executive VP - Conferences",
             "bio": "Hello. I'm Henry, and I'm the EVP of Conferences. I'm responsible for conference planning and logistics. In addition, I register members for national membership and manage club finances. Please email any questions to henryhu21108@gmail.com.",
             "image": "Henry",
+            "quote": "Takes skill to be this lucky",
         }
     ],
     "ops": [
@@ -31,24 +35,28 @@ const officers = {
             "position": "VP Member Relations",
             "bio": "Hey guys, I'm Arhan and I'm really looking forward to being this years VP of Member Relations! During my free time I love playing tennis, watching random shows, and of course watching reels. If you ever see me never be afraid to say hi, and I look forward to meeting y'all!",
             "image": "Arhan",
+           
         },
         {
             "name": "Chloe Wang",
             "position": "VP Public Relation",
             "bio": "Hi! I'm Chloe, and I'm happy to be your VP of Public Relations for this year. I love discovering and listening to music (just found out I quite enjoy live electronic music) and knowing random facts. Feel free to reach out about anything!",
             "image": "Chloe",
+           
         },
         {
             "name": "Meredith Tan",
             "position": "Treasurer",
             "bio": "Hey guys! I'm Meredith and I'm so excited to be your Treasurer this year! In my free time, I like baking bread, playing volleyball, and biking around the neighborhood. Feel free to reach out anytime with questions, or if you have fun fundraiser ideas!",
             "image": "Meredith",
+            "quote": "A smooth sea never made a skilled sailor",
         },
         {
             "name": "Vincent Wang",
             "position": "Ex-Officio / Bay Section President",
             "bio": "",
             "image": "Vincent",
+            "quote": "There are two types of people in this world: 1) those who can extrapolate from existing data",
         }
     ],
     "comps": [
@@ -57,24 +65,28 @@ const officers = {
             "position": "VP of Prepared Competitions",
             "bio": "Hey everyone! I'm Serena and I am incredibly excited to be your VP of Prepared Speaking this year! Some fun facts about me are that I love reading, watching cringy movies, and hanging out with my friends. Feel free to say hi if you see me around or reach out if you have any questions or want book recs!!",
             "image": "Serena",
+            "quote": "Just keep swimming",
         },
         {
             "name": "Sufee Kathane",
             "position": "VP of Case Competitions",
             "bio": "Hi everyone! I'm Sufee, and I'm be serving as your VP of Case Studies this year. I love reading, fencing, and (speed playing) board games. If you'd ever like to talk about case studies, school, or anything, let's chat!",
             "image": "Sufee",
+          
         },
         {
             "name": "Leah Pien",
             "position": "VP of Written Competitions",
             "bio": "Hey guys! I'm Leah and I'm your VP of Written competitions this year! In my free time, I love playing tennis, getting boba with my friends, and traveling. If you see my around feel free to say hi, and I'm looking forward to meeting everyone!",
             "image": "Leah",
+            "quote": "One step at a time",
         },
         {
             "name": "Alessandro Montesi",
             "position": "VP of Technology",
             "bio": "Hi guys! I am Alessandro, and I am super excited to be your VP of Tech for this school year! For fun I loveee listening to Clairo, reading feminist literature, and drinking matcha :)). Feel free to hit me up anytime, I'd love to talk!",
             "image": "Alessandro",
+            "quote": "There's no such thing as useless effort",
         },
     ],
     "projects": {
