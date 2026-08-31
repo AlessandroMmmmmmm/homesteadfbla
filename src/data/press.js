a7a16f25900001fe5972e4ddccee69aee3266089
Michelle Chin <mchin164@student.fuhsd.org>
2026-08-30 23:46:12 -0700

    updated press

--- src/data/press.js
+++ src/data/press.js
@@ -1,7 +1,7 @@
 let press = [
     {
         title: "National Leadership Conference 2026",
-        date: "June 2026",
+        date: "July 2026",
         desc: "",
         link: "https://drive.google.com/file/d/1QhFbJi_kRYOZCRZfCH5kpyToQaxeFnTE/view?usp=sharing",
         image: "NLC2026"
