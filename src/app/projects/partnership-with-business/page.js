00a0ccd8568b3f180c0a7b5536530ec5167ba900
Michelle Chin <mchin164@student.fuhsd.org>
2026-08-30 22:21:29 -0700

    pwb update

--- src/app/projects/partnership-with-business/page.js
+++ src/app/projects/partnership-with-business/page.js
@@ -11,7 +11,7 @@ export default function PartnershipWithBusiness() {
             <Nav />
             <div className="h-[80vh] border-2 border-dark-chocolate bg-dark-chocolate p-10 m-10 rounded-xl bg-opacity-30">
                 <h1 className="text-3xl font-bold">Partnership with Business</h1>
-                <p className="mt-3 text-lg">The Partnership with Business project aims to create a manually beneficial partnership with a company in order to provide hands-on experience to its members and benefit the company by providing services such as product feedback and marketing, taking into account the unique insight members can give as high schoolers.</p>
+                <p className="mt-3 text-lg">The Partnership with Business project aims to create a mutually beneficial partnership with a company in order to provide hands-on experience to its members and benefit the company by providing services such as product feedback and marketing, taking into account the unique insight members can give as high schoolers.</p>
             </div>
 
             <Footer />
